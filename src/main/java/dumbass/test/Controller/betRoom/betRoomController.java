4b8161210676d7ad97e53498dbbb96df9f4060fa
LEE026 <lhs456852@gmail.com>
2024-10-05 13:02:00 +0900

    배팅 룸 컨트롤러 추가

--- src/main/java/dumbass/test/Controller/betRoom/betRoomController.java
+++ src/main/java/dumbass/test/Controller/betRoom/betRoomController.java
@@ -2,8 +2,15 @@ package dumbass.test.Controller.betRoom;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
 public class betRoomController {
+    @GetMapping("/betting-room")
+    public String main_form(){
+
+        log.info("betting room");
+        return "betRoomForm";
+    }
 }
