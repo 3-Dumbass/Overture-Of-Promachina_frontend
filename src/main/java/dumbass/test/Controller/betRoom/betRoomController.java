865e5949d9532e26418b2b700f0991af9e25dc74
LEE026 <lhs456852@gmail.com>
2024-10-05 15:03:21 +0900

    로그인을 하지 않은 상태라면 방 접근 불가능하도록 수정

--- src/main/java/dumbass/test/Controller/betRoom/betRoomController.java
+++ src/main/java/dumbass/test/Controller/betRoom/betRoomController.java
@@ -1,5 +1,7 @@
 package dumbass.test.Controller.betRoom;
 
+import dumbass.test.dto.UserDto;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +10,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class betRoomController {
     @GetMapping("/betting-room")
-    public String main_form(){
-
+    public String main_form(HttpSession session){
+        UserDto userDto = (UserDto) session.getAttribute("id");
+        if(userDto==null||userDto.getUser_id()==null){
+            return "loginForm";
+        }
         log.info("betting room");
         return "betRoomForm";
     }
