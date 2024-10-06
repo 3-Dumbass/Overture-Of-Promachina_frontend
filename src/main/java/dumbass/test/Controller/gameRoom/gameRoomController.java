33cc710a57831f7f7304118353930d34ebb60e3c
LEE026 <lhs456852@gmail.com>
2024-10-06 13:35:22 +0900

    room-list에서 배팅 룸으로 입장

--- src/main/java/dumbass/test/Controller/gameRoom/gameRoomController.java
+++ src/main/java/dumbass/test/Controller/gameRoom/gameRoomController.java
@@ -1,9 +1,11 @@
 package dumbass.test.Controller.gameRoom;
 
+import dumbass.test.dto.UserDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Slf4j
@@ -11,17 +13,20 @@ import org.springframework.web.bind.annotation.PostMapping;
 public class gameRoomController {
 
     @GetMapping("/room-list")
-    public String main_form(){
-
+    public String main_form(HttpSession session){
+        UserDto userDto = (UserDto) session.getAttribute("id");
+        if(userDto==null||userDto.getUser_id()==null){
+            return "loginForm";
+        }
         log.info("room");
         return "roomForm";
     }
 
 
-    @PostMapping("/room-id")
-    public String room_id(HttpSession session, Long room_id) {
+    @GetMapping("/join/room/{room_id}")
+    public String betting_room_id(HttpSession session, @PathVariable("room_id") Long room_id) {
         session.setAttribute("room_id", room_id);
 
-        return "betRoomForm";
+        return "redirect:/betting-room";
     }
 }
