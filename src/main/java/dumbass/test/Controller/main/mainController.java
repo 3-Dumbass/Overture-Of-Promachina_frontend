e5353c989422205ecebe07cd5336064ee96133c4
heavenring <anstn0104@naver.com>
2024-10-06 19:58:27 +0900

    메인화면 보유 코인 수 표시

--- src/main/java/dumbass/test/Controller/main/mainController.java
+++ src/main/java/dumbass/test/Controller/main/mainController.java
@@ -1,25 +1,42 @@
 package dumbass.test.Controller.main;
 
+import dumbass.test.utils.dto.AccountDto;
 import dumbass.test.utils.dto.UserDto;
 import jakarta.servlet.http.HttpSession;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Controller
 public class mainController {
 
     @GetMapping("/main-form")
-    public String main_form(HttpSession session){
+    public String main_form(Model model, HttpSession session){
 
         if(session.getAttribute("id") != null){
 
             UserDto userDto = (UserDto) session.getAttribute("id");
+            model.addAttribute("coin", coin_count(userDto));
 
             log.info("session id is " + userDto);
         }
         log.info("mainform");
         return "mainForm";
+    }
+
+
+    public Long coin_count(UserDto userDto){
+        String wallet_api = linkUrl.wallet_api_url + "/wallet/amount";
+
+        Long coin = 0L;
+        for(AccountDto accountDto : userDto.getAccount()) {
+            RestTemplate restTemplate = new RestTemplate();
+            coin += restTemplate.postForEntity(wallet_api, accountDto.getAccountKey(), Long.class).getBody();
+        }
+
+        return coin;
     }
 }
